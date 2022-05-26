3b0c5f331429c4264b082ec6b46a764c8f416bfa
ImranRH321 <imranrh247@gmail.com>
2022-05-26 16:42:58 +0600

    useCart-and-dependency-use-it

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import useProducts from '../../hooks/useProducts';
+import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
 
 const Orders = () => {
-    const [products, setProducts] = useProducts()
-    return (
-        <div>
-            <h1>This is Orders {products.length}</h1>
-        </div>
-    );
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
+ console.log(cart);
+  return (
+    <div>
+      <h1>This is Orders {products.length}</h1>
+      <h3>cart items : {cart.length}</h3>
+    </div>
+  );
 };
 
 export default Orders;
